f991e38eea7aad940b7f8107fc185a00fa7949e1
Balabanov Dmitriy <test@test.test>
2021-10-03 08:31:36 +0300

    add text with tasks

--- homework1/src/HomeWorkApp.java
+++ homework1/src/HomeWorkApp.java
@@ -1,18 +1,33 @@
+// 1. Создать пустой проект в IntelliJ IDEA, создать класс HomeWorkApp, и прописать в нем метод main().
 public class HomeWorkApp {
 
     public static void main(String[] args) {
+
+        // 6. Методы из пунктов 2-5 вызовите из метода main() и посмотрите корректно ли они работают.
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
     }
 
+    /* 2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+        Orange
+        Banana
+        Apple
+     */
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
+    /* 3. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
+    и инициализируйте их любыми значениями, которыми захотите.
+    Далее метод должен просуммировать эти переменные,
+    и если их сумма больше или равна 0,
+    то вывести в консоль сообщение “Сумма положительная”,
+    в противном случае - “Сумма отрицательная”;
+     */
     public static void checkSumSign() {
         int a = -1;
         int b = 0;
@@ -23,6 +38,11 @@ public class HomeWorkApp {
         }
     }
 
+    /* 4. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
+    Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
+    если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
+    если больше 100 (100 исключительно) - “Зеленый”;
+     */
     public static void printColor() {
         int value = 101;
         if (value <= 0) {
@@ -34,6 +54,10 @@ public class HomeWorkApp {
         }
     }
 
+    /* 5. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+        и инициализируйте их любыми значениями, которыми захотите.
+        Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+     */
     public static void compareNumbers() {
         int a = -2;
         int b = -1;
